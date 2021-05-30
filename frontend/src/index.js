b20e8b145947ef4371ac5e2c6ad5cde79ac6af73
Gowrishankar <gowri.jshankar@gmail.com>
2021-05-30 20:50:12 +0530

    service worker enable

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import './index.css';
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
@@ -7,9 +9,8 @@ import { configure as MobxConfigure } from 'mobx';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { RootStoreProvider } from './app/datastores/RootStoreContext';
+import reportWebVitals from './reportWebVitals';
 import rootStore from './app/datastores/RootStore';
-
-//import reportWebVitals from './reportWebVitals';
 
 MobxConfigure({ enforceActions: true });
 
@@ -24,7 +25,13 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 //reportWebVitals(console.log);
+reportWebVitals();
